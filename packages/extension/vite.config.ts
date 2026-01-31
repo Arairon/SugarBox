c95fd0abc9c9f4ff6c732386efe3e0d550216f73
arairon <arai@arai.icu>
2026-01-31 21:32:52 +0400

    Yet another rework
    I guess we're going to v4 now :D
    
    Quick recap:
    v1: A stupid userscript based on jquery
    v2: A messy vanilla js version that went nowhere
    v3: First React based version, but with a really messy build process and
    a "Proof of concept"-grade logic
    v4: Hopefully better than v3 :D

---
+++ packages/extension/vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, "src/popup/index.html"),
+        background: resolve(__dirname, "src/background/index.ts"),
+        content: resolve(__dirname, "src/content/index.ts"),
+        overlay: resolve(__dirname, "src/overlay/index.ts"),
+        page: resolve(__dirname, "src/page/index.ts"),
+      },
+      output: {
+        // Ensure files maintain recognizable names without hashes
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
+})
