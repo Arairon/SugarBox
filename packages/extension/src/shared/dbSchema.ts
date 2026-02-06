72918308b0ece6d12dd2959329c5b01d96d20fbb
arairon <arai@arai.icu>
2026-02-06 09:41:22 +0400

    Remove redundant fields from dbSchema and prettify manifest

--- packages/extension/src/shared/dbSchema.ts
+++ packages/extension/src/shared/dbSchema.ts
@@ -2,8 +2,8 @@ export const dbSchema = {
   name: "sugarbox",
   version: 1,
   stores: {
-    games: `++id,remoteId,&uuid,name,shortname,archived,archivedAt,createdAt,updatedAt,syncStatus,syncedAt`,
-    chars: `++id,remoteId,&uuid,name,gameId,archived,archivedAt,createdAt,updatedAt,syncStatus,syncedAt`,
-    saves: `++id,remoteId,&uuid,name,hash,gameVersion,charId,gameId,createdAt,archived,archivedAt,updatedAt,syncStatus,syncedAt`,
+    games: `++id,remoteId,&uuid,name,shortname,archived,archivedAt,createdAt,updatedAt`,
+    chars: `++id,remoteId,&uuid,name,gameId,archived,archivedAt,createdAt,updatedAt`,
+    saves: `++id,remoteId,&uuid,name,hash,gameVersion,charId,gameId,archived,archivedAt,createdAt,updatedAt`,
   }
 }
